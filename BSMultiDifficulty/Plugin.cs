e2e74adf66c830bee69158fe5a2659f75a5bf53b
Jaden Down <24631328+jpdown@users.noreply.github.com>
2022-08-21 14:21:10 -0500

    Initial commit

---
+++ BSMultiDifficulty/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using IPA;
+using IPALogger = IPA.Logging.Logger;
+using SiraUtil.Zenject;
+
+namespace BSMultiDifficulty
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        [Init]
+        public Plugin(IPALogger logger, Zenjector zenjector)
+        {
+            zenjector.UseLogger(logger);
+        }
+    }
+}
